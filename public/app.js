f1ce5edf3cfa8b57b5172fbebb1f708c497df2ef
Dora Do <dora.thien.do@gmail.com>
2022-11-06 01:24:15 -0400

    flipping pages with turn.js

--- public/app.js
+++ public/app.js
@@ -1,22 +1,46 @@
-var socket = io();
+// var socket = io();
 
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+// var messages = document.getElementById('messages');
+// var form = document.getElementById('form');
+// var input = document.getElementById('input');
 
 //example
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
-    }
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     if (input.value) {
+//         socket.emit('chat message', input.value);
+//         input.value = '';
+//     }
+// });
+
+//example
+// socket.on('chat message', function(msg) {
+//     var item = document.createElement('li');
+//     item.textContent = msg;
+//     messages.appendChild(item);
+//     window.scrollTo(0, document.body.scrollHeight);
+// });
+
+$(window).ready(function() {
+    $('#yearbook').turn({
+        display: 'double',
+        acceleration: true,
+        gradients: !$.isTouch,
+        elevation:50,
+        when: {
+            turned: function(e, page) {
+                /*console.log('Current view: ', $(this).turn('view'));*/
+            }
+        }
+    });
 });
 
-//example
-socket.on('chat message', function(msg) {
-    var item = document.createElement('li');
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+
+$(window).bind('keydown', function(e){
+
+    if (e.keyCode==37)
+        $('#yearbook').turn('previous');
+    else if (e.keyCode==39)
+        $('#yearbook').turn('next');
+
 });
